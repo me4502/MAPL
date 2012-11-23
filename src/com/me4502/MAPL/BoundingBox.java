fa3ae096a112a2934b7054fc54ff172bc5e8b4ac
Madeline Miller <mnmiller1@me.com>
2012-11-23 19:10:08 +1000

    Added new methods to vector and location to allow easier conversion and relative-access, and also added a relative BoundingBox class that checks for intersecting vectors.

---
+++ src/com/me4502/MAPL/BoundingBox.java
@@ -0,0 +1,23 @@
+package com.me4502.MAPL;
+
+public class BoundingBox {
+
+	public Vector minimum;
+	public Vector maximum;
+
+	public BoundingBox(Vector min, Vector max) {
+		minimum = min;
+		maximum = max;
+	}
+
+	public boolean doesIntersect(Vector vec) {
+
+		for(int i = 0; i < vec.getDimensions(); i++) {
+			if(vec.getPoint(i) > minimum.getPoint(i) && vec.getPoint(i) < maximum.getPoint(i))
+				continue;
+			else
+				return false;
+		}
+		return true;
+	}
+}
