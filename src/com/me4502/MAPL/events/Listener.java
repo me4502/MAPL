3cdb9866dbcfdba2848cff6419eb4ed7d68f5c2d
me4502 <mnmiller1@hotmail.com>
2013-01-13 17:47:49 +1000

    Moved the Event system into the public MAPL.

---
+++ src/com/me4502/MAPL/events/Listener.java
@@ -0,0 +1,12 @@
+package com.me4502.MAPL.events;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Listener {
+
+	boolean ignoreCancelled() default true;
+	
+	boolean requireOn() default true;
+}
