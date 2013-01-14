e260d9957a00da17f94ed77e80f5e85abd751731
me4502 <mnmiller1@hotmail.com>
2013-01-14 19:30:58 +1000

    Fix warnings

--- src/com/me4502/MAPL/events/CustomClassLoader.java
+++ src/com/me4502/MAPL/events/CustomClassLoader.java
@@ -8,46 +8,46 @@ import java.util.Hashtable;
 
 public class CustomClassLoader extends ClassLoader {
 
-    private final Hashtable<String, Class> classes = new Hashtable<String, Class>();
+	private final Hashtable<String, Class<?>> classes = new Hashtable<String, Class<?>>();
 
-    public CustomClassLoader() {
-        super(CustomClassLoader.class.getClassLoader());
-    }
+	public CustomClassLoader() {
+		super(CustomClassLoader.class.getClassLoader());
+	}
 
-    public Class loadClass(String name, String url) throws ClassNotFoundException {
-        try {
-            if (classes.containsKey(name))
-                return classes.get(name);
-            URL myUrl = new URL(url);
-            URLConnection connection = myUrl.openConnection();
-            InputStream input = connection.getInputStream();
-            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-            int data = input.read();
-            while (data != -1) {
-                buffer.write(data);
-                data = input.read();
-            }
-            input.close();
-            byte[] classData = buffer.toByteArray();
+	public Class<?> loadClass(String name, String url) throws ClassNotFoundException {
+		try {
+			if (classes.containsKey(name))
+				return classes.get(name);
+			URL myUrl = new URL(url);
+			URLConnection connection = myUrl.openConnection();
+			InputStream input = connection.getInputStream();
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			int data = input.read();
+			while (data != -1) {
+				buffer.write(data);
+				data = input.read();
+			}
+			input.close();
+			byte[] classData = buffer.toByteArray();
 
-            Class classy = defineClass(name, classData, 0, classData.length);
-            if (classes.containsKey(name))
-                return classes.get(name);
-            classes.put(name, classy);
-            return classy;
-        } catch (NoClassDefFoundError e) {
-            throw new ClassNotFoundException(name);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ClassNotFoundException(name);
-        }
-    }
+			Class<?> classy = defineClass(name, classData, 0, classData.length);
+			if (classes.containsKey(name))
+				return classes.get(name);
+			classes.put(name, classy);
+			return classy;
+		} catch (NoClassDefFoundError e) {
+			throw new ClassNotFoundException(name);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ClassNotFoundException(name);
+		}
+	}
 
-    public void unloadClass(String name) {
-        classes.remove(name);
-    }
+	public void unloadClass(String name) {
+		classes.remove(name);
+	}
 
-    public void unloadAll() {
-        classes.clear();
-    }
+	public void unloadAll() {
+		classes.clear();
+	}
 }
--- src/com/me4502/MAPL/events/Event.java
+++ src/com/me4502/MAPL/events/Event.java
@@ -2,8 +2,8 @@ package com.me4502.MAPL.events;
 
 public abstract class Event {
 
-    public void start() {
-    }
+	public void start() {
+	}
 
-    public abstract RegisteredEventHandler getHandler();
+	public abstract RegisteredEventHandler<?> getHandler();
 }
