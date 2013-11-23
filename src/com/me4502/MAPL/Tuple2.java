3790ff285e923ef8233245f2583f2c003a0ea922
Madeline Miller <mnmiller1@me.com>
2013-11-23 16:50:29 +1000

    Most screens won't need an update method.

--- src/com/me4502/MAPL/Tuple2.java
+++ src/com/me4502/MAPL/Tuple2.java
@@ -1,5 +1,7 @@
 package com.me4502.MAPL;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Tuple2 - Copied from CraftBook. Stores 2 objects.
  * 
@@ -7,29 +9,34 @@ package com.me4502.MAPL;
  */
 public class Tuple2<A, B> {
 
-    public A a;
-    public B b;
+	@JsonProperty("a")
+	public A a;
+	@JsonProperty("b")
+	public B b;
 
-    public Tuple2(A a, B b) {
+	public Tuple2() {
+	}
 
-        this.a = a;
-        this.b = b;
-    }
+	public Tuple2(A a, B b) {
 
-    @Override
-    public boolean equals(Object o) {
+		this.a = a;
+		this.b = b;
+	}
 
-        return o instanceof Tuple2<?, ?> && equals((Tuple2<?, ?>) o);
-    }
+	@Override
+	public boolean equals(Object o) {
 
-    public boolean equals(Tuple2<?, ?> o) {
+		return o instanceof Tuple2<?, ?> && equals((Tuple2<?, ?>) o);
+	}
 
-        return o.a.equals(a) && o.b.equals(b);
-    }
+	public boolean equals(Tuple2<?, ?> o) {
 
-    @Override
-    public int hashCode() {
-        // Constants correspond to glibc's lcg algorithm parameters
-        return (a.hashCode() * 1103515245 + 12345 ^ b.hashCode() * 1103515245 + 12345) * 1103515245 + 12345;
-    }
+		return o.a.equals(a) && o.b.equals(b);
+	}
+
+	@Override
+	public int hashCode() {
+		// Constants correspond to glibc's lcg algorithm parameters
+		return (a.hashCode() * 1103515245 + 12345 ^ b.hashCode() * 1103515245 + 12345) * 1103515245 + 12345;
+	}
 }
